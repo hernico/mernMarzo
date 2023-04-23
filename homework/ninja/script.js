37ecdf245588a33335a983e52c0f544f5c00bb82
hernico <hernico@gmail.com>
2023-04-23 19:46:30 -0400

    [TAREA] sensei

--- homework/ninja/script.js
+++ homework/ninja/script.js
@@ -1,10 +1,10 @@
 class Ninja {
-    constructor (nombre, salud)
+    constructor (nombre, salud,velocidad,fuerza)
     {
     this.nombre = nombre;
     this.salud = salud;
-    this.velocidad = 3;
-    this.fuerza = 3;
+    this.velocidad = velocidad;
+    this.fuerza = fuerza;
     }
 
     sayName() {
@@ -19,13 +19,35 @@ class Ninja {
     }
 
     drinkSake(){
-        this.salud =10
+        this.salud = this.salud +10
     }
 }
 
-const ninja1 = new Ninja ('Hyabusa');
+const ninja1 = new Ninja ('Hyabusa',10,3,3);
 ninja1.sayName();
 ninja1.drinkSake();
 ninja1.showStats();
 
-console.log(ninja1)
+console.log(ninja1);
+
+
+// second part
+class Sensei extends Ninja {
+    constructor(nombre, salud, velocidad,fuerza){
+    super (nombre,salud, velocidad,fuerza)
+    this.sabiduria= 10
+    }
+
+    speakWisdom(){
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.")
+        ninja1.drinkSake()
+    }
+}
+const superSensei = new Sensei ('Master Splinter',200,10,10)
+superSensei.speakWisdom()
+superSensei.drinkSake()
+console.log(superSensei)
+
+
+
+
