de7424d0387848bd3d1aee1ab8c95be00b499c2c
hernico <hernico@gmail.com>
2023-05-14 20:51:09 -0400

    navbar

--- homework/colorgenerator/src/App.js
+++ homework/colorgenerator/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Colors from './components/Generator';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
 
+<Navbar/>
       <Colors/>
 
     </div>
